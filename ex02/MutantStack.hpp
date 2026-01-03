cd1dcaeb5b773a2b3c2541c2a40d1ca44e193594
Kimia <kimiaziari1997@gmail.com>
2026-01-03 18:42:55 +0100

    add iteration to stack

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,10 +1,21 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-class MutantStack
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
 {
-private:
 public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
+
+	iterator begin(){
+		return this->c.begin();
+	}
+	iterator end(){
+		return this->c.end();
+	}
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,27 +1,70 @@
+#include <iostream>
+#include <list>
 #include "MutantStack.hpp"
 
-int main()
+void testMutantStack()
 {
+	std::cout << "============== MutantStack Test ==============" << std::endl;
+
 	MutantStack<int> mstack;
+
+	std::cout << "Pushing: 5, 17" << std::endl;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+
+	std::cout << "Top element: " << mstack.top() << std::endl;
+
+	std::cout << "Pop top element" << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+
+	std::cout << "Current size: " << mstack.size() << std::endl;
+
+	std::cout << "Pushing: 3, 5, 737, 0" << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
+
+	std::cout << "Iterating MutantStack:" << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-	std::cout << *it << std::endl;
-	++it;
-	}
-	std::stack<int> s(mstack);
+
+	for (; it != ite; ++it)
+		std::cout << "  value = " << *it << std::endl;
+}
+
+void testList()
+{
+	std::cout << "\n============== std::list Test =============" << std::endl;
+
+	std::list<int> lst;
+
+	std::cout << "Pushing back: 5, 17" << std::endl;
+	lst.push_back(5);
+	lst.push_back(17);
+
+	std::cout << "Pop back element" << std::endl;
+	lst.pop_back();
+
+	std::cout << "Current size: " << lst.size() << std::endl;
+
+	std::cout << "Pushing back: 3, 5, 737, 0" << std::endl;
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	lst.push_back(0);
+
+	std::cout << "Iterating list:" << std::endl;
+	std::list<int>::iterator it = lst.begin();
+	std::list<int>::iterator ite = lst.end();
+
+	for (; it != ite; ++it)
+		std::cout << "  value = " << *it << std::endl;
+}
+
+int main()
+{
+	testMutantStack();
+	testList();
 	return 0;
 }
